45e4cc37754b2c0706fb6bd2152b4e3afd47e185
vcd <vcdwight@gmail.com>
2025-08-19 01:02:05 +0800

    [feat] add index.html

--- KestrelAIProxy/Program.cs
+++ KestrelAIProxy/Program.cs
@@ -6,8 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.UseSerilog();
 builder.Services.AddAiGatewayFundamentalComponents();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseHealthChecks("/health");
 app.UseSerilogRequestLogging();
 app.UseAiGateway();
 
