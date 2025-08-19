90587b9a3e4f4441efb60e43430fea076cf6eb53
vccddd <vcd.hai@outlook.com>
2025-08-19 10:19:10 +0800

    [feat] support Hyperbolic

---
+++ KestrelAIProxy.AIGateway/ProviderStrategies/HyperbolicStrategy.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace KestrelAIProxy.AIGateway.ProviderStrategies;
+
+public sealed class HyperbolicStrategy(
+    IResultBuilder resultBuilder,
+    ILogger<HyperbolicStrategy> logger)
+    : IProviderStrategy
+{
+    public string ProviderName => "hyperbolic";
+
+    public Task<ParseResult> ParseAsync(HttpContext context, ParsedPath parsedPath)
+    {
+        var segments = parsedPath.ProviderSegments;
+
+        logger.LogDebug("{Provider} mapping: segments=[{Segments}]", ProviderName, string.Join(", ", segments));
+
+        return Task.FromResult(resultBuilder.CreateSuccessResult(
+            providerName: ProviderName,
+            targetHost: "api.hyperbolic.xyz",
+            pathSegments: segments.Length > 0 ? segments : [],
+            queryString: parsedPath.QueryString,
+            additionalHeaders: [],
+            additionalMetadata: []));
+    }
+}
